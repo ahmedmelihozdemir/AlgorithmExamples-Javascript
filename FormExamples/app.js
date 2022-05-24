88e8f883e4101754891d5cbbd2d16422ab3f757c
Melih Özdemir <ozdemirmelihahmed@gmail.com>
2022-05-24 19:29:17 +0300

    form example done

--- FormExamples/app.js
+++ FormExamples/app.js
@@ -1,34 +1,43 @@
 let userFormDom = document.querySelector("#userForm");
 userFormDom.addEventListener("submit", formHandler);
+
+const alert = (title, message, className = "warning") => `
+<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong>${title}!</strong> ${message}.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+  </button>
+</div>
+`
+const alertDom = document.getElementById("alert");
+
 function formHandler(event) {
     event.preventDefault();
     let user_name = document.querySelector("#username");
     let puan = document.querySelector("#score");
-    addItem(user_name.value, puan.value)
+    if (user_name.value && puan.value) {
+        addItem(user_name.value, puan.value)
+        user_name.value = "";
+        puan.value = "";
+    } else {
+        alertDom.innerHTML = alert("Başlık bilgisi",
+            "Lütfen boş alan bırakmayınız!",
+            "danger"
+        );
+    }
+
 }
 
 let userListDom = document.querySelector("#userList");
-const addItem = (username,score) => {
-     let liDom = document.createElement("li");
-     liDom.innerHTML= `${username} ${score}`
-     userListDom.append(liDom);
+const addItem = (username, score) => {
+    let liDom = document.createElement("li");
+    liDom.innerHTML = `
+     ${username}
+     <span class="badge bg-primary rounded-pill">${score}</span>
+     `
+    liDom.classList.add(
+        "list-group-item", "d-flex", "justify-content-between", "align-items-center"
+    )
+    userListDom.append(liDom);
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
